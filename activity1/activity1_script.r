4d75441116df3f2046c1110762c4112b559b3c39
cschwartz1 <cschwartz1@colgate.edu>
2020-01-26 16:13:23 -0500

    Update activity1_script.r
    Writing mathematical operations in R and making vectors using variable names

--- activity1/activity1_script.r
+++ activity1/activity1_script.r
@@ -1,2 +1,17 @@
 # Activity 1
 print("hello R world")
+
+#two plus one hundred
+2+100
+
+#naming a number
+n<-6
+#divide number by 3
+n/3
+#add 4 to number
+n+4
+
+#vector of numbers
+v<-c(12,50,107,23)
+#multiply all numbers by 2
+v*2
